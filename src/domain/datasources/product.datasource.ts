6e6cd230cf5e2f62c9898b625de6465558040fef
DanielRicra <daniel.int@outlook.com>
2024-03-07 20:53:44 -0500

    Add domain/datasource folder
    - product datasource, abstract class

---
+++ src/domain/datasources/product.datasource.ts
@@ -0,0 +1,13 @@
+import { ProductDTO, UpdateProductDTO } from "../dtos/product";
+import { ProductEntity } from "../entities";
+
+export abstract class ProductDatasource {
+  abstract getProducts(): Promise<ProductEntity[]>;
+  abstract getProduct(id: number): Promise<ProductEntity>;
+  abstract updateProduct(
+    id: number,
+    updateProductDTO: UpdateProductDTO
+  ): Promise<ProductEntity>;
+  abstract deleteProduct(id: number): Promise<void>;
+  abstract addProduct(productDto: ProductDTO): Promise<ProductEntity>;
+}
