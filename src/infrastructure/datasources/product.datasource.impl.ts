305ef10edf2d74e5fef0f4b4f0d9517d716444f2
DanielRicra <daniel.int@outlook.com>
2024-03-07 20:59:52 -0500

    Add infrastructure/datasources folder
    With product datasource implementation

---
+++ src/infrastructure/datasources/product.datasource.impl.ts
@@ -0,0 +1,106 @@
+import { ProductDatasource } from "../../domain/datasources";
+import { ProductDTO, UpdateProductDTO } from "../../domain/dtos/product";
+import { ProductEntity } from "../../domain/entities";
+import { db } from "../../data/sqlite";
+import { CustomError } from "../../domain/errors";
+import { ProductMapper } from "../mappers";
+
+export class ProductDatasourceImpl implements ProductDatasource {
+  async getProducts(): Promise<ProductEntity[]> {
+    try {
+      const products = await new Promise<Object[]>((resolve, reject) => {
+        db.all("SELECT * FROM products", (err, row) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(row as Object[]);
+        });
+      });
+
+      return products.map((product: Object) =>
+        ProductMapper.productEntityFromObject(product)
+      );
+    } catch (error) {
+      if (error instanceof CustomError) throw error;
+
+      throw CustomError.internalServerError();
+    }
+  }
+
+  async getProduct(id: number): Promise<ProductEntity> {
+    throw new Error("Method not implemented.");
+  }
+
+  async updateProduct(
+    id: number,
+    { imgUrl, name, price, rating }: UpdateProductDTO
+  ): Promise<ProductEntity> {
+    try {
+      const updatedProduct = await new Promise<Object>((resolve, reject) => {
+        db.run(
+          "UPDATE products SET name=?,imgUrl=?,price=?,rating=?  WHERE id=?",
+          [imgUrl, name, price, rating, id],
+          function (err) {
+            if (err) {
+              reject(err);
+            }
+            if (this.changes === 0) {
+              reject(CustomError.notFound("Product not found"));
+            }
+            resolve({ imgUrl, name, price, rating, id });
+          }
+        );
+      });
+      return ProductMapper.productEntityFromObject(updatedProduct);
+    } catch (error) {
+      if (error instanceof CustomError) throw error;
+
+      throw CustomError.internalServerError();
+    }
+  }
+
+  async deleteProduct(id: number): Promise<void> {
+    try {
+      new Promise((resolve, reject) => {
+        db.run("DELETE FROM products WHERE id=?", [id], function (err) {
+          if (err) {
+            reject(err);
+          }
+          resolve(true);
+        });
+      });
+    } catch (error) {
+      if (error instanceof CustomError) throw error;
+
+      throw CustomError.internalServerError();
+    }
+  }
+
+  async addProduct({
+    imgUrl,
+    name,
+    price,
+    rating,
+  }: ProductDTO): Promise<ProductEntity> {
+    try {
+      const product = await new Promise<Object>((resolve, reject) => {
+        db.run(
+          "INSERT INTO products(imgUrl, name, price, rating) VALUES (?,?,?,?)",
+          [imgUrl, name, price, rating],
+          function (err: any) {
+            if (err) {
+              reject(err);
+            }
+            resolve({ id: this.lastID, name, imgUrl, price, rating });
+          }
+        );
+      });
+
+      return ProductMapper.productEntityFromObject(product);
+    } catch (error) {
+      if (error instanceof CustomError) throw error;
+
+      throw CustomError.internalServerError();
+    }
+  }
+}
