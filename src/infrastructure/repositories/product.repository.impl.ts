cc6460af788564fe6c9e23efe92fb760f5af1307
DanielRicra <daniel.int@outlook.com>
2024-03-11 14:35:14 -0500

    Update ProductRepositoryImpl
    Add parameter of PaginationDTO to 'getProducts' method.
    
    Now 'getProducts' return 'Promise<ProductListEntity>'.
    
    Pass the options parameter to 'this.productDatasource.getProducts'

--- src/infrastructure/repositories/product.repository.impl.ts
+++ src/infrastructure/repositories/product.repository.impl.ts
@@ -1,13 +1,14 @@
 import { ProductDatasource } from "../../domain/datasources";
+import { PaginationDTO } from "../../domain/dtos";
 import { UpdateProductDTO, ProductDTO } from "../../domain/dtos/product";
-import { ProductEntity } from "../../domain/entities";
+import { ProductEntity, ProductListEntity } from "../../domain/entities";
 import { ProductRepository } from "../../domain/repositories";
 
 export class ProductRepositoryImpl implements ProductRepository {
   constructor(private readonly productDatasource: ProductDatasource) {}
 
-  getProducts(): Promise<ProductEntity[]> {
-    return this.productDatasource.getProducts();
+  getProducts(options: PaginationDTO): Promise<ProductListEntity> {
+    return this.productDatasource.getProducts(options);
   }
 
   getProduct(id: number): Promise<ProductEntity> {
