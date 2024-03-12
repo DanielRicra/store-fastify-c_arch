50b408a4c5695db5cd156ce741930d30c1959929
DanielRicra <daniel.int@outlook.com>
2024-03-12 17:43:21 -0500

    Update ProductEntity
    The 'image' property has been renamed to 'imgUrl'

--- src/domain/entities/product.entity.ts
+++ src/domain/entities/product.entity.ts
@@ -2,7 +2,7 @@ export class ProductEntity {
   constructor(
     public id: number,
     public name: string,
-    public image: string,
+    public imgUrl: string,
     public price: number,
     public rating: number
   ) {}
